fda5a28b5d3a0ef8ce9f63ff84b7ba350f01b9e7
Annusza <polonistyka2@o2.pl>
2018-03-20 21:35:47 +0100

    Formatowanie TestFormularz

--- calculator/src/test/java/test/TestFormularz.java
+++ calculator/src/test/java/test/TestFormularz.java
@@ -23,53 +23,10 @@ public class TestFormularz extends Mockito {
 		String komunikat = "Metoda obliczRatyStale powinna obliczac ";
 	List<List<Double>> x = LogikaFormularza.obliczRatyStale(420000d, 6d, 3d, 0d, true);
 	assertEquals(komunikat, 69_563.77d, x.get(0).get(1), 0.01);
-//	
-//		assertEquals(komunikat, 5d, 5d, 0.01);
-		//assertEquals(komunikat, 1, Servlet.zwracam1());
+
 
 	}
 	
-//	@Test
-//	public void servlet_should_not_greet_the_user_if_the_name_is_null() throws IOException{
-//		HttpServletRequest request = mock(HttpServletRequest.class);
-//		HttpServletResponse response = mock(HttpServletResponse.class);
-//		FormularzServlet servlet = new FormularzServlet();
-//		PrintWriter writer = mock(PrintWriter.class);
-//		when(response.getWriter()).thenReturn(writer);
-//		
-//		when(request.getParameter("name")).thenReturn(null);
-//		servlet.doPost(request,response);
-//		
-//		verify(response).sendRedirect("/");
-//	}
-//	
-//	@Test
-//	public void servlet_should_not_greet_the_user_if_the_name_is_empty() throws IOException{
-//		HttpServletRequest request = mock(HttpServletRequest.class);
-//		HttpServletResponse response = mock(HttpServletResponse.class);
-//		FormularzServlet servlet = new FormularzServlet();
-//		PrintWriter writer = mock(PrintWriter.class);
-//		when(response.getWriter()).thenReturn(writer);
-//		
-//		when(request.getParameter("raty")).thenReturn("");
-//		
-//		servlet.doPost(request, response);
-//		
-//		verify(response).sendRedirect("/");
-//	}
-//	
-//	@Test
-//	public void servlet_should_not_greet_the_user_if_the_name_is_provided() throws IOException{
-//		HttpServletRequest request = mock(HttpServletRequest.class);
-//		HttpServletResponse response = mock(HttpServletResponse.class);
-//		PrintWriter writer = mock(PrintWriter.class);
-//		
-//		when(request.getParameter("name")).thenReturn("jan");
-//		when(response.getWriter()).thenReturn(writer);
-//		
-//		new FormularzServlet().doPost(request, response);
-//		verify(writer).println("<h1>Hello jan</h1>");
-//		
-//	}
+
 	
 }
